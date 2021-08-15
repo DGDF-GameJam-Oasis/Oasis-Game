fa630bccfe9f7fbdd9650549fc356621e1475f48
Okkio <60454980+ahmedhamad-okkio@users.noreply.github.com>
2021-08-15 17:02:57 +0100

    Revert "Test of NPC and mission handling."
    This reverts commit 690a93fe6d384f9f004614d4b0ab51ff95327400.

--- Assets/Scripts/NPCBehavior/NPCBehavior.cs
+++ Assets/Scripts/NPCBehavior/NPCBehavior.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class NPCBehavior : MonoBehaviour
 {
-    public NPCTextLogic mission;
     //Variables for the NPC
     private string npcName;
     private string npcMissionDialogue;
     private string npcMissionCompleteDialogue;
     private string missionName;
-    private Queue<string> paragraphs;
     // Start is called before the first frame update
     void Start()
     {
-        paragraphs = new Queue<string>();
         // Variables needed here
         // setVariables();
     }
@@ -31,17 +28,5 @@ public class NPCBehavior : MonoBehaviour
         npcMissionDialogue = startDialogue;
         npcMissionCompleteDialogue = endDialogue;
         missionName = mission;
-    }
-
-    void OnMouseDown()
-    {
-        startMission(mission);
-    }
-
-    public void startMission(NPCTextLogic missionText)
-    {
-        Debug.Log(missionText.name + ":" + missionText.missionStartDialogue);
-        Debug.Log(missionText.name + ":" + missionText.missionCompleteDialogue);
-        Debug.Log(missionText.name + ":" + missionText.missionFailedDialogue);        
     }
 }
