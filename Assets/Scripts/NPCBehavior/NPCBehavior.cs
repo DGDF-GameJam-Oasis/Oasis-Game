f8bacb6d2c69147a9e0f5fd00dd954af26770826
Kenneth3k1 <kenneth3k1@gmail.com>
2021-08-15 12:21:51 -0400

    NPC changes being updated.

--- Assets/Scripts/NPCBehavior/NPCBehavior.cs
+++ Assets/Scripts/NPCBehavior/NPCBehavior.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class NPCBehavior : MonoBehaviour
 {
+    public NPCTextLogic mission;
     //Variables for the NPC
     private string npcName;
     private string npcMissionDialogue;
     private string npcMissionCompleteDialogue;
     private string missionName;
+    private Queue<string> paragraphs;
     // Start is called before the first frame update
     void Start()
     {
+        paragraphs = new Queue<string>();
         // Variables needed here
         // setVariables();
     }
@@ -19,7 +22,7 @@ public class NPCBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void setVariables(string name, string startDialogue, string endDialogue, string mission)
@@ -28,5 +31,17 @@ public class NPCBehavior : MonoBehaviour
         npcMissionDialogue = startDialogue;
         npcMissionCompleteDialogue = endDialogue;
         missionName = mission;
+    }
+
+    void OnMouseDown()
+    {
+        startMission(mission);
+    }
+
+    public void startMission(NPCTextLogic missionText)
+    {
+        Debug.Log(missionText.name + ":" + missionText.missionStartDialogue);
+        Debug.Log(missionText.name + ":" + missionText.missionCompleteDialogue);
+        Debug.Log(missionText.name + ":" + missionText.missionFailedDialogue);
     }
 }
